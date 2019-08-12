e2674256a19d8b8c2e7d8a4450a2a78a60fcd31b
Nisarg Mehta <nisarg.mehta@in.ibm.com>
2019-08-12 16:45:33 -0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const geoCode = require('./utils/geoCode');
 
 const app = express();
+const port = process.env.PORT || 3000
 
 //Defines path for Express Config
 const publicDir = path.join(__dirname,'../public');
@@ -88,6 +89,6 @@ app.get('*',(req,res)=>{
     })
     
 })
-app.listen(3000,()=>{
-    console.log('App has started on port 3000');
+app.listen(port,()=>{
+    console.log('App has started on port ',port);
 })
